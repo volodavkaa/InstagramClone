a2e1ab34dfa5f052b38d0c2d3415741bb830df70
volodavkaa <volodavkaa@gmail.com>
2024-08-30 19:50:06 +0300

    /_1

--- InstagramClone/wwwroot/js/edit-profile.js
+++ InstagramClone/wwwroot/js/edit-profile.js
@@ -3,24 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
     var btn = document.getElementById("editProfileButton");
     var span = document.getElementsByClassName("close")[0];
 
-    // Відкрити модальне вікно при натисканні на кнопку
+    
     btn.onclick = function () {
         modal.style.display = "block";
     };
 
-    // Закрити модальне вікно при натисканні на хрестик
+    
     span.onclick = function () {
         modal.style.display = "none";
     };
 
-    // Закрити модальне вікно при натисканні на будь-яке місце поза ним
+   
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     };
 
-    // Оновлювати лічильник символів для поля біографії
+    
     var profileBio = document.getElementById("profileBio");
     var charCount = document.getElementById("charCount");
 
